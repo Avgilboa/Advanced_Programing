30074c9596a57d2386e1ba346994ba07f13fb022
shmooel28 <shmooel28@gmail.com>
2023-04-18 13:14:29 +0300

    problam with space

--- threadPoolTask/thread_pool.c
+++ threadPoolTask/thread_pool.c
@@ -7,31 +7,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include<unistd.h>
+
 int _index =0;
 
 typedef struct TextData{
    int  id;
    char *message;
+   int  key_num;
 }TextData;
 
 pthread_mutex_t mutexQueue;
 pthread_cond_t condQueue;
 
-TextData* taskQueue[10];
+TextData* taskQueue[1024];
 int taskCount = 0;
 
 void executeTask(TextData task){
-    encrypt(task.message,12);
+    encrypt(task.message,task.key_num);
     while(task.id != _index){
         pthread_cond_wait(&condQueue,&mutexQueue);
     }
 
-    printf("encripted data: %s\n",task.message);
+    //printf("encripted data: %s\n",task.message);
+    printf("%s\n",task.message);
     _index++;
     taskCount--;
     pthread_cond_signal(&condQueue);
 }
-void* worker(void* argv){
+void decryptexecuteTask(TextData task){
+    decrypt(task.message,task.key_num);
+    while(task.id != _index){
+        pthread_cond_wait(&condQueue,&mutexQueue);
+    }
+
+    printf("decripted data: %s\n",task.message);
+    _index++;
+    taskCount--;
+    pthread_cond_signal(&condQueue);
+}
+void* enWorker(void* argv){
     while(1){
         pthread_mutex_lock(&mutexQueue);
         if(_index == 10){
@@ -45,8 +60,35 @@ void* worker(void* argv){
         pthread_mutex_unlock(&mutexQueue);
     }
 }
+void* deWorker(void* argv){
+    while(1){
+        pthread_mutex_lock(&mutexQueue);
+        if(_index == 10){
+            pthread_mutex_unlock(&mutexQueue);
+            break;
+        }
+        while(taskCount == 0){
+            pthread_cond_wait(&condQueue,&mutexQueue);
+        }
+        decryptexecuteTask((*taskQueue)[_index]);
+        pthread_mutex_unlock(&mutexQueue);
+    }
+}
 
-int main(){
+int main(int argc, char *argv[]){
+    char *flag;
+    if(!strcmp(argv[1],  "-d"))
+    {
+        flag = "decrypt";
+    }
+    else if(!strcmp(argv[1],  "-e"))
+    {
+        flag = "encrypt";
+    }
+    else{
+        perror("not get flag error");
+        exit(1);
+    }
     for(int i=0; i<10; i++){
         taskQueue[i] = malloc(sizeof(TextData));
     }   
@@ -54,24 +96,40 @@ int main(){
     pthread_mutex_init(&mutexQueue, NULL);
     pthread_cond_init(&condQueue, NULL);
     int i;
-
-
-    for(i = 0; i < 4; i++){
-        pthread_create(&th[i], NULL, &worker, NULL);
+    
+    if(!strcmp(flag,  "encrypt"))
+    {
+        printf("%s\n",flag);
+        for(i = 0; i < 4; i++){
+            pthread_create(&th[i], NULL, &enWorker, NULL);
+        }
     }
-
-    for(i=0 ; i<10 ; i++){
+    else
+    {
+        printf("d %s\n",flag);
+        for(i = 0; i < 4; i++){
+            pthread_create(&th[i], NULL, &deWorker, NULL);
+        }
+    }
+    
+    int k = atoi(argv[2]);
+    i = 0;
+    while(1)
+    {
         (*taskQueue)[i].id = i;
         (*taskQueue)[i].message = malloc(1024);
-        scanf("%s",(*taskQueue)[i].message);
+        (*taskQueue)[i].key_num = k;
+        if (fgets((*taskQueue)[i].message, 1024, stdin) == NULL) {
+            // EOF
+            pthread_cond_signal(&condQueue);
+            break;
+        }
+        
+        //scanf("%s",(*taskQueue)[i].message);
         taskCount++;
+        i++;
         pthread_cond_signal(&condQueue);
     }
-
-
-
-
-
     for(i = 0; i < 4; i++){
         pthread_join(th[i], NULL);
     }
